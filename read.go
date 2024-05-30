6654177153ce5b60372c82b1eca2570f9345e5a0
Pavel Mezentsev <pvmezencev@yandex.ru>
2024-05-30 14:55:11 +0400

    read from bytes

--- read.go
+++ read.go
@@ -2,6 +2,7 @@ package ods
 
 import (
 	"archive/zip"
+	"bytes"
 	"encoding/xml"
 	"fmt"
 	"io"
@@ -16,6 +17,58 @@ func Read(filepath string) (data ODS, files *zip.ReadCloser, err error) {
 
 	// Iterate through the files within the zip archive
 	for _, file := range files.File {
+		// Open the file for processing
+		rc, err := file.Open()
+		if err != nil {
+			return ODS{}, nil, fmt.Errorf("error opening file: %v", err)
+		}
+		defer rc.Close()
+
+		// Decode the file contents into the ODS struct
+		switch file.Name {
+		case "content.xml":
+			if err = xml.NewDecoder(rc).Decode(&data.Content); err != nil {
+				return ODS{}, nil, fmt.Errorf("error decoding file: %v", err)
+			}
+		case "meta.xml":
+			if err = xml.NewDecoder(rc).Decode(&data.Meta); err != nil {
+				return ODS{}, nil, fmt.Errorf("error decoding file: %v", err)
+			}
+		case "manifest.rdf":
+			if err = xml.NewDecoder(rc).Decode(&data.Manifest); err != nil {
+				return ODS{}, nil, fmt.Errorf("error decoding file: %v", err)
+			}
+		case "settings.xml":
+			if err = xml.NewDecoder(rc).Decode(&data.Settings); err != nil {
+				return ODS{}, nil, fmt.Errorf("error decoding file: %v", err)
+			}
+		case "styles.xml":
+			if err = xml.NewDecoder(rc).Decode(&data.Styles); err != nil {
+				return ODS{}, nil, fmt.Errorf("error decoding file: %v", err)
+			}
+		case "mimetype":
+			contents, err := io.ReadAll(rc)
+			if err != nil {
+				return ODS{}, nil, fmt.Errorf("error reading file: %v", err)
+			}
+
+			data.Mimetype = Mimetype(contents)
+		}
+	}
+
+	// Return the decoded content, the zip reader (for potential further use), and any error
+	return data, files, nil
+}
+
+func ReadFromBytes(bytesData []byte) (data ODS, files *zip.ReadCloser, err error) {
+	reader := bytes.NewReader(bytesData)
+	zipReader, err := zip.NewReader(reader, int64(len(bytesData)))
+	if err != nil {
+		return ODS{}, nil, err
+	}
+
+	// Iterate through the files within the zip archive
+	for _, file := range zipReader.File {
 		// Open the file for processing
 		rc, err := file.Open()
 		if err != nil {
